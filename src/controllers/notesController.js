6af8c58ddeaa0fc4f1e5840b6cb244b4fb80922b
taisiia <twitlie@proton.me>
2026-09-19 21:22:42 +0300

    feat: pagination, filtering, celebrate validation

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -3,8 +3,31 @@ import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res, next) => {
   try {
-    const notes = await Note.find();
-    res.status(200).json(notes);
+    const page = Number(req.query.page) || 1;
+    const perPage = Number(req.query.perPage) || 10;
+    const { tag, search } = req.query;
+
+    const skip = (page - 1) * perPage;
+
+    const filter = {};
+    if (tag) {
+      filter.tag = tag;
+    }
+    if (search) {
+      filter.$or = [
+        { title: { $regex: search, $options: 'i' } },
+        { content: { $regex: search, $options: 'i' } },
+      ];
+    }
+
+    const [totalNotes, notes] = await Promise.all([
+      Note.countDocuments(filter),
+      Note.find(filter).skip(skip).limit(perPage),
+    ]);
+
+    const totalPages = Math.ceil(totalNotes / perPage);
+
+    res.status(200).json({ page, perPage, totalNotes, totalPages, notes });
   } catch (error) {
     next(error);
   }
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use(cors());
 
 app.use(notesRoutes);
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
