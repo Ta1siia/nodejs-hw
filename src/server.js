1247a98038344fb09f1b428239ffb27158fa68e1
taisiia <twitlie@proton.me>
2026-09-27 00:24:29 +0300

    hw-04: auth, sessions, cookies, private notes

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import express from 'express';
 import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +16,8 @@ const PORT = process.env.PORT || 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
-
+app.use(cookieParser());
+app.use(authRoutes);
 app.use(notesRoutes);
 app.use(notFoundHandler);
 app.use(errors());
